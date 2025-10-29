974d97c9e056d1d1bd96ae91bf4dd95a260a5be7
Bruna G. Palm <67290847+palmbg@users.noreply.github.com>
2025-10-29 15:07:11 -0300

    Add files via upload

--- demo.m
+++ demo.m
@@ -9,19 +9,19 @@ beta1 = 0.15;
 p = 0.001;
 par = [beta0,beta1];
 %
-    % %covariaveis
-    x1 = rand(1,n);
-    %
-    eta = beta0+beta1*x1; % preditor linear
-    mu = exp(eta); % media
-    %
-    
-    y = rr(mu); % variavel dependente
-    x = [x1];
-    
-    y1 = y;
-    
-    y1(1,1:r) = y1(1,1:r) + 10;
-    %
-    
-    rrfit_r(x,y1,p)
+% %covariaveis
+x1 = rand(1,n);
+%
+eta = beta0+beta1*x1; % preditor linear
+mu = exp(eta); % media
+%
+
+y = rr(mu); % variavel dependente
+x = [x1];
+
+y1 = y;
+
+y1(1,1:r) = y1(1,1:r) + 10;
+%
+
+rrfit_r(x,y1,p)
